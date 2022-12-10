cab6de37bb8d10fba5150cb396b9fd1d097ca17a
Volodymyr <vlrohmanov@gmail.com>
2022-12-10 12:34:12 +0200

    all component 2

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,3 +1,8 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import css from './Filter.module.css';
+
 export const Filter = ({ filter, onChangeFilter }) => {
   return (
     <>
